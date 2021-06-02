1bdc0c22a17baa543e9bfdccf465b77eb651af59
Craig Rushon <craig.rushon@shopify.com>
2021-06-02 16:42:04 -0400

    Got array of adjectives

--- get_adjectives.rb
+++ get_adjectives.rb
@@ -8,4 +8,18 @@ relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 
 relevant_lines = relevant_lines.reject { |line| line.include?("--") }
 
+def get_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
 puts relevant_lines
+
+adjectives = []
+
+relevant_lines.each do |line|
+  adjectives << get_adjective(line)
+end
+
+puts adjectives
