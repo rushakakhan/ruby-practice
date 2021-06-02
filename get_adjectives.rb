199fa4582e5c6455288670b24385b6e211cb38c4
Craig Rushon <craig.rushon@shopify.com>
2021-06-02 16:31:52 -0400

    Removed by-lines for review authors

--- get_adjectives.rb
+++ get_adjectives.rb
@@ -4,12 +4,8 @@ File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-relevant_lines = []
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 
-lines.each do |line|
-  if line.include?("Truncated")
-    relevant_lines << line
-  end
-end
+relevant_lines = relevant_lines.reject { |line| line.include?("--") }
 
 puts relevant_lines
