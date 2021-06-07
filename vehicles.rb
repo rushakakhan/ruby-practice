d81d1f4c18780b76bb3a07baa4505d37da7dc81b
Craig Rushon <craig.rushon@shopify.com>
2021-06-07 10:31:24 -0400

    Added vehicle super class and created subclasses

--- vehicles.rb
+++ vehicles.rb
@@ -1,4 +1,4 @@
-class Car 
+class Vehicle
   
   attr_accessor :odometer
   attr_accessor :gas_used
@@ -20,46 +20,11 @@ class Car
   end
 end
 
-class Truck
-  
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-
-  def accelerate 
-    puts "Floor it!"
-  end
-
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn front 2 wheels."
-  end
+class Car < Vehicle
 end
 
-class Motorcycle
-  
-  attr_accessor :odometer
-  attr_accessor :gas_used
+class Truck < Vehicle
+end
 
-  def mileage
-    @odometer / @gas_used
-  end
-
-  def accelerate 
-    puts "Floor it!"
-  end
-
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn front 2 wheels."
-  end
+class Motorcycle < Vehicle
 end
