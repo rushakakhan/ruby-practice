fc80ccbd2e219dcdc9e87cfdef15987aa6f405bc
Craig Rushon <craig.rushon@shopify.com>
2021-06-07 10:41:06 -0400

    Added truck method

--- vehicles.rb
+++ vehicles.rb
@@ -24,7 +24,19 @@ class Car < Vehicle
 end
 
 class Truck < Vehicle
+
+  attr_accessor :cargo
+
+  def load_bed(contents)
+    puts "Securing #{contents} in the truck bed."
+    @cargo = contents
+  end
+
 end
 
 class Motorcycle < Vehicle
 end
+
+truck = Truck.new
+truck.load_bed("259 bouncy balls")
+puts "The truck is carrying #{truck.cargo}."
