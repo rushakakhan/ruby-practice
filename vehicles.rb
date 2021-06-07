40abf990ccc0f4ce1fc1006cf148f8666373803c
Craig Rushon <craig.rushon@shopify.com>
2021-06-07 11:18:12 -0400

    Added steer method to motorcycle class to override steer method from supercalss

--- vehicles.rb
+++ vehicles.rb
@@ -35,8 +35,14 @@ class Truck < Vehicle
 end
 
 class Motorcycle < Vehicle
+  def steer
+    puts "Turn front wheel."
+  end
 end
 
 truck = Truck.new
 truck.load_bed("259 bouncy balls")
 puts "The truck is carrying #{truck.cargo}."
+
+moto = Motorcycle.new
+moto.steer
