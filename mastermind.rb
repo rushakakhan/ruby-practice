6af1554e9e5c9fc47a89d4194f9dbb3775c83e87
Craig Rushon <craig.rushon@shopify.com>
2021-06-07 12:36:49 -0400

    COmmiting all random changes

--- mastermind.rb
+++ mastermind.rb
@@ -3,9 +3,10 @@ second = rand(6) + 1
 third = rand(6) + 1
 fourth = rand(6) + 1
 
-code = first.to_s + second.to_s + third.to_s + fourth.to_s
-
+##code = first.to_s + second.to_s + third.to_s + fourth.to_s
 puts "Mastermind"
+
+code = "1234"
 
 guesses = 1
 
@@ -16,14 +17,17 @@ until input == code
   guess_arr = input.split("")
   code_arr = code.split("")
   output = ""
+  matches = []
   guesses += 1
 
   guess_arr.each_with_index do |guess, index|
     guess_index = code_arr.index(guess)
-    if guess_index == index
+    if guess_index == index && matches.include?(guess) == false
       output += "X"
-    elsif guess_index
+      matches << guess
+    elsif guess_index && matches.include?(guess) == false
       output += "x"
+      matches << guess
     end
   end
 
--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -2,39 +2,54 @@ puts "Rock Paper Scissors"
 
 valids = ["Rock", "Paper", "Scissors"]
 resolved = false
+winner = nil
+choices = []
+player1 = ""
+player2 = ""
 
 until resolved
-  print "Player 1 - Enter your selection: "
-  player1 = gets.chomp.capitalize
+  win_string = ""
 
-  print "Player 2 - Enter your selection: "
-  player2 = gets.chomp.capitalize
+  if winner == 2
+    choices[0] = player2
+    choices[1] = player1
+  else
+    print "Player 1 - Enter your selection: "
+    player1 = gets.chomp.capitalize
 
-  unless valids.include?(player1) && valids.include?(player2)
-    puts "No cheaters! Only Rock, Paper or Scissors are allowed."
-    next
+    print "Player 2 - Enter your selection: "
+    player2 = gets.chomp.capitalize
+
+    unless valids.include?(player1) && valids.include?(player2)
+      puts "No cheaters! Only Rock, Paper or Scissors are allowed."
+      next
+    end
+
+    if player1 == player2
+      puts "Tie!" 
+      next
+    end
+
+    choices[0] = player1
+    choices[1] = player2
   end
 
-  if player1 == player2
-    puts "Tie!" 
-    next
-  end
-
-  choices = [player1, player2]
-  winner = nil
-
-  case choices.sort
+  case choices
   when ["Paper", "Rock"]
-    puts "Paper covers rock."
-    winner = choices.index("Paper") + 1
-  when ["Paper", "Scissors"]
-    puts "Scissors cuts paper."
-    winner = choices.index("Scissors") + 1
+    win_string = "Paper covers rock."
+    winner = 1 unless winner == 2
+  when ["Scissors", "Paper"]
+    win_string = "Scissors cuts paper."
+    winner = 1 unless winner == 2
   when ["Rock", "Scissors"]
-    puts "Rock crushes scissors."
-    winner = choices.index("Rock") + 1  
+    win_string = "Rock crushes scissors."
+    winner = 1 unless winner == 2
+  else
+    winner = 2
+    next
   end
   resolved = true
 end
 
+puts win_string
 puts "Player #{winner} wins!"
