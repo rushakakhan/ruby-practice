d50d0b7fcc1f4d86139b446ed3bf2e19b2320f90
Craig Rushon <craig.rushon@shopify.com>
2021-06-08 17:38:06 -0400

    Added subclasses

---
+++ employees.rb
@@ -0,0 +1,71 @@
+class Employee
+
+  attr_reader :name
+
+  def name=(name)
+    if name == ""
+      raise "Name can't be blank!"
+    end
+    @name = name
+  end
+
+  def print_name
+    puts "Name: #{@name}" 
+  end
+end
+
+class SalariedEmployee < Employee
+
+  attr_reader :salary
+
+  def salary=(salary)
+    if salary < 0
+      raise "A salary of #{salary} isn't valid!"
+    end
+    @salary = salary
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = (@salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+
+class HourlyEmployee < Employee
+
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "A hourly_wage of #{hourly_wage} isn't valid!"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "Hours per week can't be #{hours_per_week}"
+    end
+    @hours_per_week = hourly_wage
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = @hourly_wage * @hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+
+salaried_employee = SalariedEmployee.new
+salaried_employee.name = "Jane Doe"
+salaried_employee.salary = 50000
+salaried_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 40
+hourly_employee.print_pay_stub
