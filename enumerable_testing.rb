c847154aace4da063a5bdd5848a945b72ae8f7d6
Craig Rushon <craig.rushon@shopify.com>
2021-06-11 11:44:47 -0400

    Testing out some enumerable methods on the word splitter

--- enumerable_testing.rb
+++ enumerable_testing.rb
@@ -4,14 +4,25 @@ class WordSplitter
   attr_accessor :string
 
   def each
-    string.split(' ').each { |word| yield word }
+    string.split(' ').each do |word| 
+      yield word 
+    end
   end
 end
 
 
 splitter = WordSplitter.new
-splitter.string = "How do you do?"
+splitter.string = "How do you do"
 
 p splitter.find_all { |word| word.include?('d') }
 p splitter.reject { |word| word.include?('d') }
-p splitter.map { |word| word.upcase }
+p splitter.map { |word| word.reverse }
+
+puts "\n"
+puts "------------------"
+puts "\n"
+
+p splitter.any? { |word| word.include?("e") }
+p splitter.count
+p splitter.first
+p splitter.sort
