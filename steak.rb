dca9f5171d6b73a287940b1040bf7fc3f75f24da
Craig Rushon <craig.rushon@shopify.com>
2021-06-11 11:30:39 -0400

    Added spaceship operator method to steak class

--- steak.rb
+++ steak.rb
@@ -3,8 +3,14 @@ class Steak
 
   attr_accessor :grade
 
-  def > (other)
-    GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+  def <=> (other)
+    if GRADE_SCORES[grade] < GRADE_SCORES[other.grade]
+      return -1
+    elsif GRADE_SCORES[grade]== GRADE_SCORES[other.grade]
+      return 0
+    else
+      return 1
+    end
   end
 end
 
@@ -13,6 +19,5 @@ first_steak.grade = "Prime"
 second_steak = Steak.new
 second_steak.grade = "Choice"
 
-if first_steak > second_steak
-  puts "I'll take #{first_steak.inpect}"
-end
+puts first_steak <=> second_steak
+puts second_steak <=> first_steak
