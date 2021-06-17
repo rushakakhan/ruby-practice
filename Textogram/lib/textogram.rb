430cfe0b40fa4f7733a9f380e01780d227547f72
Craig Rushon <craig.rushon@shopify.com>
2021-06-17 18:30:28 -0400

    Updated with tests at bottom of file

--- Textogram/lib/textogram.rb
+++ Textogram/lib/textogram.rb
@@ -1,28 +1,19 @@
 class Textogram
 
-  def initialize(text, case_sensitive = false, include_special_chars = false)
-    if text == ""
-      raise "Text cannot be blank!"
-    end
+  def initialize(text, case_sensitive = false, include_special_chars = false, words_instead_of_chars = false)
     @text = text
     @case_sensitive = case_sensitive
     @include_special = include_special_chars
-    @char_hash = Hash.new
-    @word_hash = Hash.new
+    @words_instead = words_instead_of_chars
+    if text == ""
+      raise "Text cannot be blank!"
+    else
+      @hash = generate_textogram(text)
+    end
   end
 
-  def chars_to_hash
-    if @char_hash.empty?
-      generate_histogram(@text)
-    end
-    @char_hash
-  end
-
-  def words_to_hash
-    if @word_hash.empty?
-      generate_histogram(@text, true)
-    end
-    @word_hash
+  def to_hash
+    @hash
   end
 
   def to_s
@@ -37,16 +28,18 @@ class Textogram
 
   private
 
-  def generate_histogram(text, for_words = false)
+  def generate_textogram(text)
     hash = Hash.new
-    text = text.gsub(/[^0-9A-Za-z]/, '') unless @include_special
+    text = text.gsub(/[^0-9A-Za-z]/, ' ') unless @include_special
     raise "No valid characters to work with." if text == ""
     text.downcase! unless @case_sensitive
-    if for_words
-      chars = text.split(" ").sort
+
+    if @words_instead
+      splitter = " "
     else
-      chars = text.split("").sort
+      splitter = ""
     end
+    chars = text.split(splitter).sort
 
     chars.each do |c|
       next if c == " "
@@ -62,5 +55,24 @@ class Textogram
   end
 end
 
+test_default_chars = Textogram.new("HELLO World!")
+test_default_chars_with_case = Textogram.new("HELLO World!", true)
+test_default_chars_with_special = Textogram.new("HELLO World!", false, true)
+test_default_chars_with_both = Textogram.new("HELLO World!", true, true)
+
+words_default = Textogram.new("HELLO World! My name is World! I mean Craig...my name is CRAIG.", false, false, true)
+words_case_sensitive = Textogram.new("HELLO World! My name is World! I mean Craig...my name is CRAIG.", true, false, true)
+words_special_chars = Textogram.new("HELLO World! My name is World! I mean Craig...my name is CRAIG.", false, true, true)
+words_with_both = Textogram.new("HELLO World! My name is World! I mean Craig...my name is CRAIG.", true, true, true)
+
+p test_default_chars.to_hash
+p test_default_chars_with_case.to_hash
+p test_default_chars_with_special.to_hash
+p test_default_chars_with_both.to_hash
+
+p words_default.to_hash
+p words_case_sensitive.to_hash
+p words_special_chars.to_hash
+p words_with_both.to_hash
 
 
